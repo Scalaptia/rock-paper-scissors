46647a3f9c6f1aeca98e9175f05ecae8c26f8731
Scalaptia <fernando@haroware.com>
2022-02-11 15:01:47 -0800

    Add restart at game end

--- script.js
+++ script.js
@@ -13,63 +13,66 @@ function computerPlay() {
 // Play a whole round
 function playRound(playerSelection) {
     computerPlay();
+
+    // Logic for declaring winner
     switch(playerSelection) {
     case 'ROCK':
         if (computerSelection === 'PAPER') {
             winner.innerText = 'Computer Wins!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
             computerPoints +++ 1;
-            
         } else if (computerSelection === 'SCISSORS') {
             winner.innerText = 'You Win!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
             playerPoints +++ 1;
-            
         } else
             winner.innerText = 'Its a draw!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
         break;
 
     case 'PAPER':
         if (computerSelection === 'SCISSORS') {
             winner.innerText = 'Computer Wins!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
             computerPoints +++ 1;
         } else if (computerSelection === 'ROCK') {
             winner.innerText = 'You Win!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
             playerPoints +++ 1;
         } else
             winner.innerText = 'Its a draw!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
         break;
 
     case 'SCISSORS':
         if (computerSelection === 'ROCK') {
             winner.innerText = 'Computer Wins!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
             computerPoints +++ 1;
         } else if (computerSelection === 'PAPER') {
             winner.innerText = 'You Win!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
             playerPoints +++ 1;
         } else
             winner.innerText = 'Its a draw!';
-            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}.`;
+            playout.innerText = `You chose ${playerSelection}, Computer chose ${computerSelection}`;
         break;
     }
     roundsPlayed +++ 1;
     updateScore();
 
+    // Declare winner
     if (computerPoints === 5) {
         let winner = 'COMPUTER'
-        alert(`GAME OVER! ${winner} WINS!`);
+        alert(`Game Over! ${winner} WINS!. Press OK to restart`);
+        restartGame();
     }
-
     if (playerPoints === 5) {
         let winner = 'PLAYER'
-        alert(`GAME OVER! ${winner} WINS!`);
+        alert(`Game Over! ${winner} WINS!. Press OK to restart`);
+        restartGame();
     }
+    
 }
 
 // UI
@@ -87,6 +90,15 @@ function updateScore() {
     playerScore.innerText = `Player Score = ${playerPoints}`
     computerScore.innerText = `Computer Score = ${computerPoints}`
     round.innerText = `Round ${roundsPlayed}`
+}
+
+function restartGame(){
+    playerPoints = 0;
+    computerPoints = 0;
+    roundsPlayed = 0;
+    updateScore();
+    winner.innerText = 'Choose your weapon!';
+    playout.innerText = `First to 5 points wins`;
 }
 
 rockBtn.addEventListener('click', () => playRound('ROCK'));
